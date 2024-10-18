69037c2d3b9f6722ffa57140a67992dda906c1d5
Alessandro-netizen <alessandrom12@nycstudents.net>
2024-10-18 12:35:46 -0400

    Update WordChecker.java

--- WordChecker.java
+++ WordChecker.java
@@ -1,1 +1,13 @@
-
+import java.util.ArrayList;
+public class WordChecker {
+    public WordChecker(ArrayList<String> list)
+    {
+        wordList = list;
+    }
+    wordList = new ArrayList<String>();
+    ArrayList<String> words = new ArrayList<String>();
+    words.add("an");
+    words.add("band");
+    words.add("band"); 
+    words.add("abandon");
+     
