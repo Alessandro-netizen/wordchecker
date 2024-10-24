9fa11030a5cf994eaa3cc98a049c31eea42cfdb0
Alessandro-netizen <alessandrom12@nycstudents.net>
2024-10-24 12:34:55 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,18 @@
 import java.util.ArrayList;
-
 public class main {
 
 
     public static void main(String[] args)
     {
         WordChecker w = new WordChecker();
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon");
+        
     }
-    public WordChecker()
-    {
-        wordList = new ArrayList<String>();
-    }
-    words.add("an");
-    words.add("band");
-    words.add("band");
-    words.add("abandon");
+    
+    
     
 }
-
-
-
