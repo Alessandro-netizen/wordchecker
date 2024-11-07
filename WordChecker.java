c5d5ae646476f20f2c52d924bb88f8dfc936fe23
Alessandro-netizen <alessandrom12@nycstudents.net>
2024-11-07 12:21:59 -0500

    Update WordChecker.java

--- WordChecker.java
+++ WordChecker.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 
 public class WordChecker {
         private ArrayList<String> wordList;
+
         public boolean isWordChain()
         {
-                for(int i = 1; i < wordList.size(); i++) {
-                        String after = wordList.get(i);
-                        String before = wordList.get(i-1);
-                        if(after.indexOf(before)< 0) return false;  
+                for(int i = 0; i < (wordList.size()-1); i++) {
+                        if( !(wordList.get(i+1).contains(wordList.get(i)))) { 
+                                return false;
+                        }
                 }
                 return true;
         }       
@@ -20,13 +21,22 @@ public class WordChecker {
         {
                 wordList = new ArrayList<String>();
         }
-        public ArrayList<String> createList (String target) {
-                ArrayList<String> list = new ArrayList<String>();
-                
+        public ArrayList<String> createList(String target) {
+                ArrayList<String> resultList = new ArrayList<String>();
+                for(int i = 0; i < wordList.size(); i++) {
+                        if (wordList.get(i).length() >= target.length()) {
+                                if (wordList.get(i).substring(0, target.length()).equals(target)) { 
+                                        resultList.add(wordList.get(i).substring(target.length(), wordList.get(i).length()));
+                                }
+                        }
+                        
+                  }
+                return reultList;
+        }               
 
-        }
        
         
 
         
+}
 }      
