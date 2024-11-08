6ac5698d25d1e44ea6b104429a961708a709dc80
Alessandro-netizen <alessandrom12@nycstudents.net>
2024-11-08 12:29:45 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,54 @@
 import java.util.ArrayList;
-public class main {
-
-
+import java.util.Arrays;
+public class Main {
     public static void main(String[] args)
     {
-        WordChecker w = new WordChecker();
-        ArrayList<String> words = new ArrayList<String>();
-        words.add("an");
-        words.add("band");
-        words.add("band");
-        words.add("abandon");
+        ArrayList<String> words1 = new ArrayList<String>(Arrays.asList("an", "band", "band", "abandon"));
+        WordChecker check1 = new WordChecker(words1);
         
+        ArrayList<String> words2 = new ArrayList<String>(Arrays.asList("to", "too", "stool", "tools"));
+        WordChecker check2 = new WordChecker(words2);
+
+        ArrayList<String> words3 = new ArrayList<String>(Arrays.asList("catch", "bobcat", "catchacat", "cat", "rat"));
+        WordChecker check3 = new WordChecker(words3);
+
+
+
+        System.out.println(check1.isWordChain());
+        System.out.println(check2.isWordChain());
+        System.out.println(check3.createList("cat"));
+        System.out.println(check3.createList("catch"));
+        System.out.println(check3.createList("dog"));
     }
     
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
